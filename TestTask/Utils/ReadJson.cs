5cff0b576c2bd7b58ab341c22be67d1961696dd5
Konstantin-BV <kosty.bolotovsky@gmail.com>
2024-04-18 13:10:15 +0300

    FixErrors

--- TestTask/Utils/ReadJson.cs
+++ TestTask/Utils/ReadJson.cs
@@ -10,7 +10,7 @@ namespace TestTask.Utils
 {
     static class ReadJson
     {
-        const string fileName = "data.json";
+        const string fileName = "Pallets.json";
         static public Pallets[] ReadPallets()
         {
             string json = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName));
